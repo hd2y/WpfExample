6a86250aac290518f997b79db59d2f65f97b60b0
hd2y <hd2y@outlook.com>
2020-04-24 15:55:29 +0800

    布局控件

---
+++ src/WpfExample/Controls/00Layout/Thumb.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExample.Controls._00Layout
+{
+    /// <summary>
+    /// Thumb.xaml 的交互逻辑
+    /// </summary>
+    public partial class Thumb : UserControl
+    {
+        public Thumb()
+        {
+            InitializeComponent();
+        }
+
+        private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            System.Windows.Controls.Primitives.Thumb thumb = (System.Windows.Controls.Primitives.Thumb)sender;
+            double top = System.Windows.Controls.Canvas.GetTop(thumb) + e.VerticalChange;
+            double left = System.Windows.Controls.Canvas.GetLeft(thumb) + e.HorizontalChange;
+            
+            //防止Thumb控件被拖出容器。
+            if (top <= 0)
+                top = 0;
+            if (left <= 0)
+                left = 0;
+            if (this.Parent is Window window)
+            {
+                if (top >= (window.Height - SystemParameters.CaptionHeight - thumb.Height))
+                    top = window.ActualHeight - SystemParameters.CaptionHeight - thumb.Height;
+                if (left >= (window.ActualWidth - thumb.Width))
+                    left = window.ActualWidth - thumb.Width;
+            }
+            
+            System.Windows.Controls.Canvas.SetTop(thumb, top);
+            System.Windows.Controls.Canvas.SetLeft(thumb, left);
+            tt.Text = $"Top: {top}\nLeft: {left}";
+        }
+    }
+}
--- src/WpfExample/MainWindow.xaml.cs
+++ src/WpfExample/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace WpfExample
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 反射获取所有用户控件
+            Type[] types = typeof(MainWindow).Assembly.GetTypes();
+            List<Type> userControlTypes = new List<Type>();
+            foreach (var type in types)
+            {
+                if (typeof(UserControl).IsAssignableFrom(type))
+                {
+                    userControlTypes.Add(type);
+                }
+            }
+
+            // 添加 TabControl 存放用户控件
+            if (this.Content is Grid grid)
+            {
+                TabControl tabControl = new TabControl();
+                foreach (var catgory in userControlTypes.Select(t => t.FullName.Split('.')[2]).Distinct())
+                {
+                    var tabItem = new TabItem { Header = catgory };
+                    WrapPanel wrapPanel = new WrapPanel();
+
+                    foreach (var uc in userControlTypes.Where(t => t.FullName.Contains(catgory)).Select(t => Activator.CreateInstance(t)).Cast<UserControl>())
+                    {
+                        Button button = new Button { Content = uc.GetType().FullName.Split('.')[3] };
+                        button.Click += (buttonSender, buttonE) =>
+                        {
+                            Window window = new Window();
+                            window.Content = uc;
+                            window.ShowDialog();
+                        };
+                        wrapPanel.Children.Add(button);
+                    }
+
+                    tabItem.Content = wrapPanel;
+                    tabControl.Items.Add(tabItem);
+                }
+                grid.Children.Add(tabControl);
+            }
+        }
     }
 }
